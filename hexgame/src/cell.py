2628485ca25facba70a803fe963a40a82efe6696
Gianpiero Cea <GianpieroCea@users.noreply.github.com>
2023-02-12 18:38:10 +0100

    made cell hashable so can be used in set

--- hexgame/src/cell.py
+++ hexgame/src/cell.py
@@ -4,14 +4,24 @@ from hexgame.src.color import Color
 __author__      = "Gianpiero Cea"
 
 class Cell:
-    def __init__(self,x :int,y:int,color : Color = None):
-        self._is_empty :bool = True
+    def __init__(self,x :int,y:int,color : Color = Color.Empty):
+        self._is_empty :bool = color == Color.Empty
         self.color : Color = color
         self.x : int = x
         self.y : int = y
 
     def __repr__(self) -> str:
         return "({x},{y})- Color:{color}".format_map({"x":self.x,"y":self.y,"color":self.color})
+
+    def __str__(self) -> str:
+        #TODO: str is for pretty display-can make repr more dev like?
+        return self.__repr__()
+
+    def __hash__(self) -> int:
+        return hash(str(self))
+
+    def __eq__(self, __o: 'Cell') -> bool:
+        return self.x == __o.x and self.y == __o.y and self.color == __o.color
 
     @property
     def is_empty(self) -> bool:
