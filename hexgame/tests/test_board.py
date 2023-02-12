e5f7033838b046e1242336c373718a82d6ac3c7d
dinies <edoardo.ghini@live.it>
2023-02-12 13:37:35 +0000

    use fixtures in unit tests of board

--- hexgame/tests/test_board.py
+++ hexgame/tests/test_board.py
@@ -5,29 +5,23 @@ import pytest
 
 class TestBoardProperties:
 
-    @pytest.fixture
-    def small_board(self):
-        return Board.Board(2, 3)
-
-    @pytest.fixture
-    def medium_board(self):
-        return Board.Board(3, 3)
-
     def test_create_empty_board(self):
         empty_board = Board.Board(0, 0)
         assert len(empty_board._board) == 0
 
-    def test_create_small_board(self, small_board):
+    def test_create_small_board(self):
+        small_board = Board.Board(2, 3)
         assert len(small_board._board) == 2
         assert len(small_board._board[0]) == 3
-        assert small_board.has_cell(Cell(0, 0))
-        assert small_board.has_cell(Cell(1, 0))
-        assert small_board.has_cell(Cell(0, 1))
-        assert small_board.has_cell(Cell(1, 1))
-        assert small_board.has_cell(Cell(0, 2))
-        assert small_board.has_cell(Cell(1, 2))
+        assert small_board.has_cell((0, 0))
+        assert small_board.has_cell((1, 0))
+        assert small_board.has_cell((0, 1))
+        assert small_board.has_cell((1, 1))
+        assert small_board.has_cell((0, 2))
+        assert small_board.has_cell((1, 2))
 
-    def test_neighbours_of_cell(self, small_board, medium_board):
+    def test_neighbours_of_cell(self):
+        small_board = Board.Board(2, 3)
         from_bottom_left_corner = small_board.find_neighbours((0, 0))
         assert len(from_bottom_left_corner) == 3
         assert Cell(0, 1) in from_bottom_left_corner
@@ -43,7 +37,8 @@ class TestBoardProperties:
         assert Cell(1, 1) in from_left_edge
         assert Cell(1, 2) in from_left_edge
         assert Cell(0, 2) in from_left_edge
-        from_centre = medium_board.find_neighbours((1, 1))
+        mediumBoard = Board.Board(3, 3)
+        from_centre = mediumBoard.find_neighbours((1, 1))
         assert len(from_left_edge) == 6
         assert Cell(0, 0) in from_centre
         assert Cell(1, 0) in from_centre
