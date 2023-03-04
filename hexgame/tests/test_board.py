01578d7b322ddae7f477cdee68d645da6fe482d4
Gianpiero Cea <GianpieroCea@users.noreply.github.com>
2023-03-04 11:29:17 +0100

    fixed tests

--- hexgame/tests/test_board.py
+++ hexgame/tests/test_board.py
@@ -1,17 +1,31 @@
 from hexgame.src.board import Board
 from hexgame.src.cell import Cell
 from hexgame.src.color import Color
+from hexgame.src.unionfind import UnionFind
 import pytest
 
 
 class TestBoardProperties:
 
     def test_create_empty_board(self):
-        empty_board = Board(0, 0)
+        dim_x = 0
+        dim_y = 0
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        empty_board = Board(dim_x=dim_x, dim_y=dim_y,
+                            red_conn_comp=uf_red, blue_conn_comp=uf_blue)
         assert len(empty_board._board) == 0
 
     def test_empty_cells_on_blank_board(self):
-        small_board = Board(3, 3)
+        dim_x = 3
+        dim_y = 3
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        small_board = Board(dim_x=dim_x, dim_y=dim_y,
+                            red_conn_comp=uf_red, blue_conn_comp=uf_blue)
+
         for i in range(3):
             for j in range(3):
                 assert small_board[i, j].is_empty
@@ -19,7 +33,13 @@ class TestBoardProperties:
     @pytest.mark.parametrize("test_input",
                              [(0, 0), (1, 0), (0, 1), (1, 1), (0, 2), (1, 2)])
     def test_create_small_board(self, test_input: tuple[int, int]):
-        small_board = Board(2, 3)
+        dim_x = 2
+        dim_y = 3
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        small_board = Board(dim_x=dim_x, dim_y=dim_y,
+                            red_conn_comp=uf_red, blue_conn_comp=uf_blue)
         assert len(small_board._board) == 2
         assert len(small_board._board[0]) == 3
         assert small_board.has_cell(test_input)
@@ -34,7 +54,9 @@ class TestBoardProperties:
                              )]
                              )
     def test__str__empty_board(self, board_sizes: tuple[int, int], expected: str):
-        board = Board(board_sizes[0], board_sizes[1])
+        fake_uf = UnionFind([(1, 1)])
+        board = Board(dim_x=board_sizes[0], dim_y=board_sizes[1],
+                      red_conn_comp=fake_uf, blue_conn_comp=fake_uf)
         print(str(board))
         print(expected)
         assert str(board) == expected
@@ -65,9 +87,17 @@ class TestBoardProperties:
     )
     def test_neighbours_of_cell(self, board_size: str, tile: tuple[int, int], expected: set[tuple[int, int]]):
         if board_size == "small":
-            board = Board(2, 3)
+            dim_x = 2
+            dim_y = 3
         elif board_size == "medium":
-            board = Board(3, 3)
+            dim_x = 3
+            dim_y = 3
+
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        board = Board(dim_x=dim_x, dim_y=dim_y,
+                      red_conn_comp=uf_red, blue_conn_comp=uf_blue)
 
         nbrs = board.find_neighbours(tile)
         assert nbrs == expected
@@ -75,24 +105,46 @@ class TestBoardProperties:
     # place_stone
 
     def test_place_stone_on_empty_board(self):
-        board = Board(3, 3)
+        dim_x = 3
+        dim_y = 3
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        board = Board(dim_x=dim_x, dim_y=dim_y,
+                      red_conn_comp=uf_red, blue_conn_comp=uf_blue)
         board.place_stone(1, 2, color=Color.Red)
 
         assert board[1, 2] == Cell(1, 2, Color.Red)
 
     def test_place_stone_already_filled(self):
         with pytest.raises(ValueError):
-            board = Board(3, 3)
+            dim_x = 3
+            dim_y = 3
+            nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+            uf_red = UnionFind(nodes)
+            uf_blue = UnionFind(nodes)
+            board = Board(dim_x=dim_x, dim_y=dim_y,
+                          red_conn_comp=uf_red, blue_conn_comp=uf_blue)
             board.place_stone(1, 2, color=Color.Red)
 
             board.place_stone(1, 2, color=Color.Blue)
 
     def test_update_connected_comps(self):
+        dim_x = 3
+        dim_y = 3
+        nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+        uf_red = UnionFind(nodes)
+        uf_blue = UnionFind(nodes)
+        board = Board(dim_x=dim_x, dim_y=dim_y,
+                      red_conn_comp=uf_red, blue_conn_comp=uf_blue)
+        assert len(board.blue_conn_comp) == 9
+        print(board.blue_conn_comp)
         color = Color.Red
-        node = (1, 1)
-        board = Board(3, 3)
-        assert len(board.connected_components_blue) == 1
-        print(board.connected_components_blue)
-        board._update_connected_components(node[0], node[1], color)
-        assert len(board.connected_components_blue) == 1
-        assert len(board.connected_components_red) == 2
+        board.place_stone(1, 1, color)
+        print(str(board))
+        assert len(board.blue_conn_comp) == 9
+        assert len(board.red_conn_comp) == 9
+        board.place_stone(1, 2, color)
+        print(str(board))
+        assert len(board.blue_conn_comp) == 9
+        assert len(board.red_conn_comp) == 8
