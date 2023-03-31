5190d7ea324a3361c56a5d7fa27ff949bccb487e
Gianpiero Cea <GianpieroCea@users.noreply.github.com>
2023-03-31 14:28:21 +0200

    added pyproject.toml for buiding, added __main__

---
+++ hexgame/__main__.py
@@ -0,0 +1,21 @@
+from hexgame.color import Color
+from hexgame.board import Board
+from hexgame.player import Player
+from hexgame.unionfind import UnionFind
+from hexgame.game import Game
+
+if __name__ == "__main__":
+    dim_x = 5
+    dim_y = 5
+    nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
+    uf_red = UnionFind(nodes)
+    uf_blue = UnionFind(nodes)
+    board = Board(dim_x=dim_x, dim_y=dim_y,
+                  red_conn_comp=uf_red, blue_conn_comp=uf_blue)
+    player_1 = Player(mode=Player.PlayerMode.Keyboard)
+    player_2 = Player(color=Color.Blue, mode=Player.PlayerMode.Keyboard)
+    game = Game(board=board,
+                player_1=player_1,
+                player_2=player_2
+                )
+    game.start()
--- hexgame/game.py
+++ hexgame/game.py
@@ -3,7 +3,6 @@
 from hexgame.color import Color
 from hexgame.board import Board
 from hexgame.player import Player
-from hexgame.unionfind import UnionFind
 import enum
 __author__ = "Gianpiero Cea"
 
@@ -52,21 +51,3 @@ class Game:
         else:
             # TODO:move this to a logger/cli
             print(str(self.current_player.color)+' wins')
-
-
-if __name__ == "__main__":
-
-    dim_x = 5
-    dim_y = 5
-    nodes = [(x, y) for y in range(dim_y) for x in range(dim_x)]
-    uf_red = UnionFind(nodes)
-    uf_blue = UnionFind(nodes)
-    board = Board(dim_x=dim_x, dim_y=dim_y,
-                  red_conn_comp=uf_red, blue_conn_comp=uf_blue)
-    player_1 = Player(mode=Player.PlayerMode.Keyboard)
-    player_2 = Player(color=Color.Blue, mode=Player.PlayerMode.Keyboard)
-    game = Game(board=board,
-                player_1=player_1,
-                player_2=player_2
-                )
-    game.start()
